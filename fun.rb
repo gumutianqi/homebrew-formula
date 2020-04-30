3958a2291749da7fc3911cac01cd5c5708f56dd6
古拉里 <gumutianqi@gmail.com>
2020-04-30 19:56:10 +0800

    upgrade funcraft version to release
    upgrade funcraft version to 3.6.11

--- fun.rb
+++ fun.rb
@@ -1,9 +1,9 @@
  class Fun < Formula
   desc "Fun is a development tool for serverless applications of aliyun."
   homepage "https://github.com/alibaba/funcraft"
-  version "3.6.9"
+  version "3.6.11"
   url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos.zip"
-  sha256 "6a216d8064d63956dc0ba1ca8f3ee60d99225b1c6ae01e3c404011c451593d7f"
+  sha256 "973954ee2c49eccc13921b19d4d7c0fb5599002659069b5446759304fce1164d"
 
 
   def install
